defbe0ac8e1c6bee543d2bdca4f86f927af7ab43
Yariv <ceo@ykapps.com>
2013-01-24 14:06:42 +0200

    v0 - complete error page

--- application/controllers/Nerdeez_Controller_Action.php
+++ application/controllers/Nerdeez_Controller_Action.php
@@ -66,39 +66,11 @@ abstract class Nerdeez_Controller_Action extends Zend_Controller_Action{
      * @var Array 
      */
     protected $_aParams = array(
-        array('name' => 'model' , 'type' => Nerdeez_ParamTypes::STRING , 'length' => 50) ,
-        array('name' => 'error' , 'type' => Nerdeez_ParamTypes::STRING , 'length' => 200) ,
-        array('name' => 'status' , 'type' => Nerdeez_ParamTypes::STRING , 'length' => 200) ,
+        array('name' => 'error_reason' , 'type' => Nerdeez_ParamTypes::STRING , 'length' => 300) ,
+        array('name' => 'error' , 'type' => Nerdeez_ParamTypes::STRING , 'length' => 300) ,
+        array('name' => 'error_description' , 'type' => Nerdeez_ParamTypes::STRING , 'length' => 300) ,
         array('name' => 'message' , 'type' => Nerdeez_ParamTypes::STRING , 'length' => 300) ,
-        array('name' => 'description' , 'type' => Nerdeez_ParamTypes::STRING , 'length' => 150) ,
-        array('name' => 'image' , 'type' => Nerdeez_ParamTypes::STRING , 'length' => 300) ,
-        array('name' => 'website' , 'type' => Nerdeez_ParamTypes::STRING , 'length' => 300) ,
-        array('name' => 'coursefolder' , 'type' => Nerdeez_ParamTypes::STRING , 'length' => 100) ,
-        array('name' => 'serial' , 'type' => Nerdeez_ParamTypes::INTEGER , 'min' => 0 , 'max' => 99999) ,
-        array('name' => 'page' , 'type' => Nerdeez_ParamTypes::INTEGER , 'min' => 0 , 'max' => 99999) ,
-        array('name' => 'folder_papa' , 'type' => Nerdeez_ParamTypes::INTEGER , 'min' => -1 , 'max' => 99999) ,
-        array('name' => 'hw_number' , 'type' => Nerdeez_ParamTypes::INTEGER , 'min' => -1 , 'max' => 99999) ,
-        array('name' => 'papa' , 'type' => Nerdeez_ParamTypes::INTEGER , 'min' => -2 , 'max' => 99999) ,
-        array('name' => 'id' , 'type' => Nerdeez_ParamTypes::INTEGER , 'min' => -1 , 'max' => 0) ,
-        array('name' => 'forget_id' , 'type' => Nerdeez_ParamTypes::INTEGER , 'min' => -1 , 'max' => 0) ,
-        array('name' => 'register_status' , 'type' => Nerdeez_ParamTypes::INTEGER , 'min' => -1 , 'max' => 50) ,
-        array('name' => 'login_status' , 'type' => Nerdeez_ParamTypes::INTEGER , 'min' => -1 , 'max' => 50) ,
-        array('name' => 'courses_id' , 'type' => Nerdeez_ParamTypes::INTEGER , 'min' => -1 , 'max' => 0) ,
-        array('name' => 'universities_id' , 'type' => Nerdeez_ParamTypes::INTEGER , 'min' => -1 , 'max' => 0) ,
-        array('name' => 'folders_id' , 'type' => Nerdeez_ParamTypes::INTEGER , 'min' => -1 , 'max' => 0) ,
-        array('name' => 'folder' , 'type' => Nerdeez_ParamTypes::INTEGER , 'min' => 0 , 'max' => 0) ,
-        array('name' => 'starttime' , 'type' => Nerdeez_ParamTypes::INTEGER , 'min' => 0 , 'max' => 0) ,
-        array('name' => 'search' , 'type' => Nerdeez_ParamTypes::STRING , 'length' => 300) ,
-        array('name' => 'password' , 'type' => Nerdeez_ParamTypes::STRING , 'length' => 20) ,
-        array('name' => 'repassword' , 'type' => Nerdeez_ParamTypes::STRING , 'length' => 20) ,
-        array('name' => 'disposition' , 'type' => Nerdeez_ParamTypes::STRING , 'length' => 20) ,
-        array('name' => 'email' , 'type' => Nerdeez_ParamTypes::STRING , 'length' => 100) ,
         array('name' => 'title' , 'type' => Nerdeez_ParamTypes::STRING , 'length' => 100) ,
-        array('name' => 'token' , 'type' => Nerdeez_ParamTypes::STRING , 'length' => 20) ,
-        array('name' => 'is_activated' , 'type' => Nerdeez_ParamTypes::STRING , 'length' => 6) ,
-        array('name' => 'ids' , 'type' => Nerdeez_ParamTypes::JSONARRAYNUMBERS , 'length' => 150 , 'min' => 0 , 'max' => 0) ,
-        array('name' => 'folders' , 'type' => Nerdeez_ParamTypes::JSONARRAYNUMBERS , 'length' => 150 , 'min' => 0 , 'max' => 0) ,
-        
     );
     
     /**
@@ -124,15 +96,24 @@ abstract class Nerdeez_Controller_Action extends Zend_Controller_Action{
             //sanitize integer 
             if ($iType === Nerdeez_ParamTypes::INTEGER){
                 if (!is_numeric($iValue)){
-                    $this->_redirector->gotoUrl('/index/index/error/' . urlencode('ERROR: Invalid params'));
+                    $this->_redirector->gotoSimple('error', 'error', NULL, array(
+                        'title'         => 'Invalid Params',
+                        'message'       => 'You gave invalid params',
+                    ));
                     return;
                 }
                 if ( $iValue <= $iMin){
-                    $this->_redirector->gotoUrl('/index/index/error/' . urlencode('ERROR: Invalid params'));
+                    $this->_redirector->gotoSimple('error', 'error', NULL, array(
+                        'title'         => 'Invalid Params',
+                        'message'       => 'You gave invalid params',
+                    ));
                     return;
                 }
                 if ($iMax > 0 && $iValue > $iMax){
-                    $this->_redirector->gotoUrl('/index/index/error/' . urlencode('ERROR: Invalid params'));
+                    $this->_redirector->gotoSimple('error', 'error', NULL, array(
+                        'title'         => 'Invalid Params',
+                        'message'       => 'You gave invalid params',
+                    ));
                     return;
                 }
             }
@@ -140,7 +121,10 @@ abstract class Nerdeez_Controller_Action extends Zend_Controller_Action{
             //sanitize string
             if ($iType === Nerdeez_ParamTypes::STRING || $iType === Nerdeez_ParamTypes::JSONARRAYNUMBERS){
                 if ($this ->sanitize_Title($iValue, $iLength) === NULL){
-                    $this->_redirector->gotoUrl('/index/index/error/' . urlencode('ERROR: Invalid params'));
+                    $this->_redirector->gotoSimple('error', 'error', NULL, array(
+                        'title'         => 'Invalid Params',
+                        'message'       => 'You gave invalid params',
+                    ));
                     return;
                 }
                 
@@ -150,15 +134,24 @@ abstract class Nerdeez_Controller_Action extends Zend_Controller_Action{
                     if (is_array($aIds)){
                         foreach($aIds as $iId){
                             if (!is_numeric($iId)){
-                                $this->_redirector->gotoUrl('/index/index/error/' . urlencode('ERROR: Invalid params'));
+                                $this->_redirector->gotoSimple('error', 'error', NULL, array(
+                                    'title'         => 'Invalid Params',
+                                    'message'       => 'You gave invalid params',
+                                ));
                                 return;
                             }
                             if ( $iId <= $iMin){
-                                $this->_redirector->gotoUrl('/index/index/error/' . urlencode('ERROR: Invalid params'));
+                                $this->_redirector->gotoSimple('error', 'error', NULL, array(
+                                    'title'         => 'Invalid Params',
+                                    'message'       => 'You gave invalid params',
+                                ));
                                 return;
                             }
                             if ($iMax > 0 && $iId > $iMax){
-                                $this->_redirector->gotoUrl('/index/index/error/' . urlencode('ERROR: Invalid params'));
+                                $this->_redirector->gotoSimple('error', 'error', NULL, array(
+                                    'title'         => 'Invalid Params',
+                                    'message'       => 'You gave invalid params',
+                                ));
                                 return;
                             }
                         }
@@ -176,6 +169,15 @@ abstract class Nerdeez_Controller_Action extends Zend_Controller_Action{
         //set the layout
         $layout = new Zend_Layout();
         $layout->setLayoutPath(APPLICATION_PATH . '/layouts/scripts/default.phtml');
+        
+        //if there is error values redirect them to the error page
+        if(isset($this->_aData['error_reason'])){
+            $this->_redirector->gotoSimple('error', 'error', NULL, array(
+                'title'     => $this->_aData['error_reason'],
+                'message'   => $this->_aData['error_description'],
+            ));
+            return;
+        }
     }
     
     /**
